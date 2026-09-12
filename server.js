bd5406f271dbb39e23af1c39c787de6ce12e2853
ef10res <floreserick10@gmail.com>
2026-09-12 16:59:15 -0600

    database

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use("/", require("./routes"));
+app.get('/test', (req, res) => {
+    res.send('Route works');
+});
 
-app.listen(port, () => {console.log(`Server is running on port ${port}`);});
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => { console.log(`Database is listening and node Running on port ${port}`) });
+    }
+});
